f910465a438f633c28055fe1bad31670425494ac
Doug Phung <Doug@chillportal.com>
2013-06-07 17:27:32 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WishPanda3::Application.config.secret_token = '5ec2714c91ba22a6d191e6cea2fbec2e772d16be1066e7141d0d7a5163bfb33f8058a12d5384b0ea90c5afff34f64b0a6d40a2227db6338a15da9e058562a396'
